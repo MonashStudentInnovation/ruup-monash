cb0dd86270c36947948fc0ebcab513b964ba905c
callistusystan <callistusystan@gmail.com>
2017-11-22 09:10:43 +1100

    Css fix for frontpage

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <div className="App" style={{display: "flex", flexDirection: "column"}}>
+        <div className="App" style={{display: "flex", flexDirection: "column", height: '100%'}}>
           <header className="App-header">  
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
