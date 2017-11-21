83005e579e07f333cd9c2556e2edc540f020ff08
lorderikir <ericjiang97@gmail.com>
2017-11-22 09:44:34 +1100

    added popover component to statuscard

--- src/components/Cards/StatusCard/index.js
+++ src/components/Cards/StatusCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { findDOMNode } from 'react-dom'
 import {
   Typography,
   Card,
@@ -8,25 +9,78 @@ import {
 import CloudOKIcon from 'mdi-material-ui/CloudCheck'
 import CloudNotOKIcon from 'mdi-material-ui/CloudOffOutline'
 import CloudIcon from 'mdi-material-ui/CloudOutline'
+import { withStyles } from 'material-ui/styles'
 
 const iconStyle = {
   verticalAlign: 'center', 
   width: 35, 
   height: 35
 }
+
+const styles = theme => ({
+  paper: {
+    padding: theme.spacing.unit,
+  },
+  popover: {
+    pointerEvents: 'none',
+  },
+  popperClose: {
+    pointerEvents: 'none',
+  },
+});
+
 class QuickStatusCard extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      anchorEl: null,
+      popperOpen: false,
+    }
+  }
+  
+  handlePopoverOpen = event => {
+    this.setState({ anchorEl: event.target });
+  };
+
+  handlePopoverClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   statusIcon(status){
     switch(status){
       case "ok":
-        return <CloudOKIcon style={{...iconStyle, color: "#4caf50"}}/>
+        return <CloudOKIcon 
+          style={{...iconStyle, color: "#4caf50"}}
+          onMouseOver={this.handlePopoverOpen} 
+          onMouseOut={this.handlePopoverClose}/>
       case "outage":
-        return <CloudNotOKIcon style={{...iconStyle, color: "#d32f2f"}}/>
+        return <CloudNotOKIcon 
+          style={{...iconStyle, color: "#d32f2f"}}
+          onMouseOver={this.handlePopoverOpen} 
+          onMouseOut={this.handlePopoverClose}/>
       default:
-        return <CloudIcon style={{...iconStyle, color: "#90a4ae"}}/>
+        return <CloudIcon 
+          style={{...iconStyle, color: "#90a4ae"}}
+          onMouseOver={this.handlePopoverOpen} 
+          onMouseOut={this.handlePopoverClose}/>
+    }
+  }
+
+
+  handleStatusTitle(status){
+    switch(status){
+      case "ok":
+        return "Everything is OK!!!"
+      case "outage":
+        return "We know something is broken! Our team is looking at it!"
+      default:
+        return "We are not sure what is happening, but we will get back to you ASAP!"
     }
   }
   render(){
-    const { status, type, serviceName} = this.props
+    const { status, type, serviceName, classes, statusMessage, statusTitle } = this.props
+    const { anchorEl, popperOpen } = this.state;
+    const open = !!anchorEl;
     return (
       <Card style={{textAlign: 'left'}}>
         <CardContent style={{display: 'flex', flexFlow: "row wrap"}}>
@@ -41,10 +95,38 @@ class QuickStatusCard extends React.Component {
           <div style={{minHeight: '100%', alignItems: 'center', display: 'flex'}}>
             {this.statusIcon(status)}
           </div>
+      
         </CardContent>
+        <Popover
+          className={classes.popover}
+          classes={{
+            paper: classes.paper,
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          onRequestClose={this.handlePopoverClose}
+        >
+          <div style={{padding: '1em'}}>
+            <Typography type="title">
+              {!statusTitle ? this.handleStatusTitle(status) : statusTitle}
+            </Typography>
+            <Typography type="subheading" gutterBottom>
+              {statusMessage && statusMessage}
+            </Typography>
+
+          </div>
+        </Popover>
       </Card>
     )
   }
 }
 
-export default QuickStatusCard
+export default withStyles(styles)(QuickStatusCard)
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   "APP_VERSION": "0.6.5-alpha10",
-  "APP_NAME": "monPlan Status"
+  "APP_NAME": "Monash Status Pages"
 }
 
 export default config
