b6d99b009f979c43b4813ff475bfdc849707911f
lorderikir <ericjiang97@gmail.com>
2017-11-22 08:40:39 +1100

    finished router setup

--- src/app/index.js
+++ src/app/index.js
@@ -6,33 +6,24 @@ import {
 } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-
+import history from './history'
+import HomePage from '../components/Pages/Home/'
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <div className="App">
           <header className="App-header">  
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
           <Switch>
+            <Route exact path="/" component={HomePage} />
           </Switch>
         </div>
       </Router>
     );
   }
 }
-
-
-// <div className="App">
-// <header className="App-header">
-//   <img src={logo} className="App-logo" alt="logo" />
-//   <h1 className="App-title">Welcome to React</h1>
-// </header>
-// <p className="App-intro">
-//   To get started, edit <code>src/App.js</code> and save to reload.
-// </p>
-// </div>
 
 export default App;
