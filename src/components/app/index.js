16f7e1e61374b1d1f2e6fd081001c9d61bde2a13
callistusystan <callistusystan@gmail.com>
2017-11-22 09:35:18 +1100

    Merged conflicts

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -32,6 +32,7 @@ class App extends Component {
                 <Route exact path="/" component={HomePage} />
               </Switch>
             </div>
+            <Footer/>
           </div>
         </Router>
       </Provider>
