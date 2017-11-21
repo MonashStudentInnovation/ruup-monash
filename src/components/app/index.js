b884da86b0a1161b3a56d5b1ae67bce45b52787d
Charlie C <charlie@dyn-49-127-96-9.its.monash.edu.au>
2017-11-22 09:08:40 +1100

    Updated app bar

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,13 +6,18 @@ import {
 } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import history from './history'
-import HomePage from '../Pages/Home/'
+import history from './history';
+import HomePage from '../Pages/Home/';
+
+import HeaderBar from '../HeaderBar';
+
+
 class App extends Component {
   render() {
     return (
       <Router history={history}>
         <div className="App">
+          <HeaderBar />
           <header className="App-header">  
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
