3482831d0f3a1d90deba44336a2bcc102dfba47c
lorderikir <ericjiang97@gmail.com>
2017-11-22 09:00:12 +1100

    added footer

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -8,18 +8,22 @@ import logo from './logo.svg';
 import './App.css';
 import history from './history'
 import HomePage from '../Pages/Home/'
+import Footer from '../containers/Footer/'
 class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <div className="App">
+        <div className="App" style={{display: "flex", flexDirection: "column"}}>
           <header className="App-header">  
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-          </Switch>
+          <div style={{flex: 1}}>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
       </Router>
     );
