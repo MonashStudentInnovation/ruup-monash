59203130eaced8e5923ed20671a9d3688b69a505
callistusystan <callistusystan@gmail.com>
2017-11-22 11:38:33 +1100

    Implemented update service method

--- src/actions/ServicesAction.js
+++ src/actions/ServicesAction.js
@@ -13,5 +13,29 @@ export function getServices() {
                 });
                 dispatch({ type: GET_SERVICES, payload: services });
             });
+        db.collection("services")
+            .onSnapshot((snapshot) => {
+                const services = snapshot.docs.map(doc => {
+                    return {id: doc.id, ...doc.data()};
+                });
+                dispatch({ type: GET_SERVICES, payload: services });
+            });
     }
 }
+
+export function updateService(id, value) {
+    return (dispatch) => {
+        const db = firebase.firestore();
+
+        db.collection("services")
+            .doc(id)
+            .set(value, { merge: true })
+            .then(() => {
+                console.log("update done!");
+            })
+            .catch(e => {
+                console.log("update failed!");
+            })
+    }
+}
+
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,2 @@
 export * from './ServicesAction';
+export * from './ServicesAction';
