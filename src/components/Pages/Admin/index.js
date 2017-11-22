6d44b7f470b565268eb55b76e8b096433f658494
lorderikir <ericjiang97@gmail.com>
2017-11-22 13:31:27 +1100

    updating Incidents on admin

--- src/components/Pages/Admin/index.js
+++ src/components/Pages/Admin/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import {
+  Grid,
+  Typography
+} from 'material-ui'
+import CreateNewIncident from './CreateNewIncident'
 
 class AdminPage extends React.Component {
+  
   render(){
    return ( 
-      <div>
-        This is the administration page
+      <div style={{padding: '1em'}}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography type="headline">
+              Status Administration
+            </Typography>
+            <CreateNewIncident />
+          </Grid>
+        </Grid>
       </div>
     )
   }
